0f3ee52e82d32840124c855344166df144b84aa9
Kauan Victor <Kauan.victor@iesb.edu.br>
2024-05-21 00:21:27 -0300

    Criando teste /musicas/GET close #12

--- API-BACKEND/tests/router_musicas.test.js
+++ API-BACKEND/tests/router_musicas.test.js
@@ -21,5 +21,26 @@ describe('API  Spotify', () => {
         expect(result.status).toBe(422);
         expect(result.type).toBe("application/json")
     })
+    test("Deve retornar 200 e um JSON array para GET /musicas", async () => {
+        const result = await request
+            .get("/musicas");
+        expect(result.status).toBe(200);
+        expect(result.type).toBe("application/json");
+        if (result.body.length > 0) {
+            id = result.body[0]._id.toString();
+        }
+    })
+    test("Deve retornar 200 e um JSON para GET /musicas/id", async () => {
+        const result = await request
+            .get(`/musicas/${id}`)
+        expect(result.status).toBe(200);
+        expect(result.type).toBe("application/json")
+    })
+    test("Deve retornar 404 e um JSON para GET /musicas/id", async () => {
+        const result = await request
+            .get(`/musicas/id`);
+        expect(result.status).toBe(404);
+        expect(result.type).toBe("application/json")
+    })
 
 })
