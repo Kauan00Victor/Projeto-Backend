1722e34259e716e8036b660545cde5f075d14754
Kauan Victor <Kauan.victor@iesb.edu.br>
2024-05-21 00:18:51 -0300

    Criando rota /musicas/GET close #9

--- API-BACKEND/routes/router_musicas.js
+++ API-BACKEND/routes/router_musicas.js
@@ -6,6 +6,9 @@ const router = express.Router();
 
 router.post("/", controllerMusicas.validar, controllerMusicas.criar);
 
+router.get("/", controllerMusicas.listarTodos);
+
+router.get("/:id", controllerMusicas.buscarPeloId, controllerMusicas.obter);
 
 
 module.exports = router;
